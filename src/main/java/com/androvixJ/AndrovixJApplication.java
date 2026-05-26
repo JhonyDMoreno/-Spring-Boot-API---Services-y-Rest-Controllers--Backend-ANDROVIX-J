d4e1b74f7be63164b1283ff525161e6ff98ee973
Javier Pruebas <moxiyoshida@gmail.com>
2026-05-26 11:38:33 -0500

    Subiendo base del proyecto

---
+++ src/main/java/com/androvixJ/AndrovixJApplication.java
@@ -0,0 +1,14 @@
+package com.androvixJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndrovixJApplication
+{
+	public static void main(String[] args)
+    {
+		SpringApplication.run(AndrovixJApplication.class, args);
+	}
+
+}
